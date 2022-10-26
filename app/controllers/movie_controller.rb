e8fdd5734195ca714d0d2795a38f44ecee110dd4
Florencia Lema <flor.lema222@gmail.com>
2022-10-26 16:04:59 -0300

    controller movies

---
+++ app/controllers/movie_controller.rb
@@ -0,0 +1,49 @@
+class MovieController < ApplicationController
+  # GET /movies
+  def index
+    @movies = Movie.all
+
+    render json: @movies
+  end
+
+  # GET /movies/1
+  def show
+    render json: @movie
+  end
+
+  # POST /movies
+  def create
+    @movie = Movie.new(movie_params)
+
+    if @movie.save
+      render json: @movie, status: :created, location: @movie
+    else
+      render json: @movie.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /movies/1
+  def update
+    if @movie.update(movie_params)
+      render json: @movie
+    else
+      render json: @movie.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /movies/1
+  def destroy
+    @movie.destroy
+  end
+
+  private
+
+  def set_movie
+    @movie = Movie.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def movie_params
+    params.require(:movie).permit(:title, :creation_date, :rate, :genre_id)
+  end
+end
