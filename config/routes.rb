e260b74497331102515e9cbf8a74335e3782a503
Florencia Lema <flor.lema222@gmail.com>
2022-12-06 09:35:12 -0300

    token done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :items
   namespace :api do
     namespace :v1 do
       resources :movies
@@ -6,4 +7,5 @@ Rails.application.routes.draw do
       resources :characters
     end
   end
+  post 'authenticate', to: 'authentication#authenticate'
 end
